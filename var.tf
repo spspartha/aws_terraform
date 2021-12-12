f216f401b794b3cae28b7f50a0f9e9ec4acb11f3
Parthasarathy Sivaraman <spspartha@gmail.com>
2021-12-12 16:52:54 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,15 +1,3 @@
-variable "AWS_ACCESS_KEY" {
-  default = "ASIA6CCGUDWJQPHH3LJQ"
-}
-
-variable "AWS_SECRET_KEY" {
-   default = "dGjmB+qpWpA5j5czx5NzCkWZIBF0D7Sbe/m1N02s"
-}
-
-variable "AWS_TOKEN" {
-   default = "FwoGZXIvYXdzEFQaDL0ZcKRKVjuim2BidyK0AfQ8wDnrKR5+wtBFmlUmDJsJxTl5iMplaGd0EKnbX+CWmKHBIwyMBFNuzOM3M/oqjZqEYa5Ta14SHLnnEgTxILbL6NC7BaCMRp1KsiyPe4fCoDoBHssvX07lF4ixQ8oWkGWZlZ4SbYMfqAIAzcdqBCI/wSOHV2fZgYF2DsSfmLcZzmpwhbugloQrbOFWLCWQUl9Kin2F2n4DyIAmo+jZ7X06Z+NfKBgUtJlgzQGpUddUyG43rCj7n9eNBjItLXQROlrPlEK/In/BIK3P9/lS6vRqynSWyv1SIQM+nSRizwFj4a9E90vIcy+t"
-}
-
 variable "aws_region" {
   description = "The AWS region to create things in."
   default     = "us-east-2"
